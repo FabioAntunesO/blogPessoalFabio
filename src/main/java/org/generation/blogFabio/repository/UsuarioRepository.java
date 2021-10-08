7889ea843db83997f575bff89d87b5f52a6b4faa
Fábio Antunes <fabio.fao03@gmail.com>
2021-10-07 21:34:15 -0300

    alteracoes

--- src/main/java/org/generation/blogFabio/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogFabio/repository/UsuarioRepository.java
@@ -7,7 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	public Optional<Usuario> findByUsuario(String usuario);
-	
-	
-
 }
