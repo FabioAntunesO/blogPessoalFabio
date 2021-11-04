3223cf1ed50f1ef6d78ba5954396a3750b1b12e4
Fábio Antunes <fabio.fao03@gmail.com>
2021-11-04 09:48:12 -0300

    update camada service

--- src/main/java/org/generation/blogFabio/service/UsuarioService.java
+++ src/main/java/org/generation/blogFabio/service/UsuarioService.java
@@ -40,6 +40,9 @@ public class UsuarioService {
 
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
+				user.get().setFoto(usuario.get().getFoto());
+				user.get().setTipo(usuario.get().getTipo());
+				
 				return user;
 			}
 		}
