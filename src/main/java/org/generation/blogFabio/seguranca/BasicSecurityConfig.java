85da5050424973b58b7d211de011f26be46ebca7
Fábio Antunes <fabio.fao03@gmail.com>
2021-11-11 14:32:01 -0300

    update BasicSecurityConfig

--- src/main/java/org/generation/blogFabio/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogFabio/seguranca/BasicSecurityConfig.java
@@ -37,8 +37,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll() 
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.GET ,"/tema").permitAll()
 		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
